1599f99115fa1b419ce41a2fa045dda7747d9691
magmaj8610 <agnieszkajuszczak86@gmail.com>
2018-05-11 08:50:13 +0200

    aktualka

--- Application/src/app/invoice/Invoice.ts
+++ Application/src/app/invoice/Invoice.ts
@@ -1,4 +1,13 @@
 export class Invoice {
   id: number;
   name: string;
+  adress: string;
+  nip: number;
+  regon: number;
+  countnumber: number;
+  quantity: string;
+  price: number;
+  payment: string;
+  date: string;
+
 }
