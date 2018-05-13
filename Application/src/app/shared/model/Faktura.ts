7c072b5cc7c2c1fe1611ff12f8a2a8b425333f77
maju <maju@ailleron.com>
2018-05-13 09:58:42 +0200

    next

--- Application/src/app/shared/model/Faktura.ts
+++ Application/src/app/shared/model/Faktura.ts
@@ -1,12 +1,14 @@
 import {Pozycja} from "./Pozycja";
+import {Uzytkownik} from "./Uzytkownik";
+import {Klient} from "./Klient";
 
 export class Faktura {
   id:number;
   dataWystawienia:Date;
   dataPlatnosci:Date;
   formaPlatnosci:string;
-  sprzedawca:string;
-  klient:string;
+  sprzedawca:Uzytkownik;
+  klient:Klient;
   nrFaktury:number;
   pozycje: Array<Pozycja>;
   uwagi:string;
